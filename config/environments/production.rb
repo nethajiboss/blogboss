11dd493d3ba09042a2043984e56639cb820137c1
nethaji <nethaji@railsfactory.org>
2012-03-08 10:40:36 +0530

    config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,9 @@ Blog::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
+  config.assets.compile = true
+config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = true
   # Generate digests for assets URLs
   config.assets.digest = true
 
