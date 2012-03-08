a9d3a8b3484ce7a87f5ad2d85db312d6b7d09b98
nethaji <nethaji@railsfactory.org>
2012-03-08 12:59:58 +0530

    change the javascripts file

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,3 +1,12 @@
-$(function() {
-alert("Dom is loaded");
+$(function() { $(".pagination a").live("click",function() {  
+$.get(this.href,null,null,"script"); 
+return false;  
+}); 
 });
+
+$(function() {
+  $("#entries_search input").keyup(function() {
+    $.get($("#entries_search").attr("action"), $("#entries_search").serialize(), null, "script");
+    return false;
+  });
+});	
