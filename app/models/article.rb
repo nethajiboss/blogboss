0a290000af7493ccaf565145614a0ceb849de1ee
nethajiboss <chandirabose@yahoo.co.in>
2012-03-09 11:22:56 +0530

    change the article file

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,6 @@ class Article < ActiveRecord::Base
   scope :draft, where("articles.published_at IS NULL")
   scope :recent, lambda{published.where("articles.published_at > ?", 2.days.ago.to_date)}
   scope :where_title, lambda{ |term| where("articles.title LIKE ?", "%#{term}%")}
-  
   def published?
     published_at.present?
   end
