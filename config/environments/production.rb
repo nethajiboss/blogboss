1267ecccb8f0ffe4b1362ea4a279af3d8055f5cc
nethajiboss <chandirabose@yahoo.co.in>
2012-03-12 12:02:25 +0530

    change email confirmation in blogboss.heroku.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,16 +16,14 @@ Blog::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   :location => '/usr/sbin/sendmail',
+  #   :arguments => '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.smtp_settings = {
-	:address => "smtp.gmail.com" ,
-	:port => 587,
-	:domain => "domain.of.sender.net" ,
-	:authentication => "plain" ,
-	:user_name => "chandirabose@gmail.com" ,
-	:password => "palanimurugan" ,
-	:enable_starttls_auto => true }
   # Generate digests for assets URLs
   config.assets.digest = true
 
